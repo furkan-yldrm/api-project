f928d383b75455d1fa194514fe8bc40e3d40b75a
Furkan Yıldırım <yildirim.frkn@outlook.com>
2025-03-04 11:31:44 +0300

    entities again

---
+++ api-project.webApi/Entities/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace api_project.webApi.Entities
+{
+    public class Image
+    {
+
+        public int ImageId { get; set; }
+        
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ api-project.webApi/Entities/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace api_project.webApi.Entities
+{
+    public class Review
+    {
+
+        public int TestimonialId { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
