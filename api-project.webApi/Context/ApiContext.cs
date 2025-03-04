e1cbdc0cc82056fb2e3487a9d17966b43ab059c2
Furkan Yıldırım <yildirim.frkn@outlook.com>
2025-03-04 12:49:15 +0300

    dbcontext

---
+++ api-project.webApi/Context/ApiContext.cs
@@ -0,0 +1,24 @@
+﻿using api_project.webApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_project.webApi.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=FURKAN;initial catalog=ApiWebDb;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
--- api-project.webApi/Entities/Review.cs
+++ api-project.webApi/Entities/Review.cs
@@ -3,7 +3,7 @@
     public class Review
     {
 
-        public int TestimonialId { get; set; }
+        public int ReviewId { get; set; }
         public string NameSurname { get; set; }
         public string Title { get; set; }
         public string Comment { get; set; }
