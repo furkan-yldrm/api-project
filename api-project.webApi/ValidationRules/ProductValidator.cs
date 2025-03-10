c6ee2e46938aec71225009dc2fd0deb5bff8d23c
Furkan Yıldırım <yildirim.frkn@outlook.com>
2025-03-10 15:36:52 +0300

    product validation

---
+++ api-project.webApi/ValidationRules/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using api_project.webApi.Entities;
+using FluentValidation;
+
+namespace api_project.webApi.ValidationRules
+{
+    public class ProductValidator:AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("name required!");
+            RuleFor(x => x.ProductName).MinimumLength(2).WithMessage("at least 2 characters!");
+            RuleFor(x => x.ProductName).MaximumLength(50).WithMessage("max 50 characters!");
+
+            RuleFor(x => x.Price).NotEmpty().WithMessage("price required!")
+                .LessThan(0).WithMessage("price can't be 0!");
+
+            RuleFor(x => x.ProductDescription).NotEmpty().WithMessage("description required!");
+        }
+    }
+}
