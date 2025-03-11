c88841eaaefb060c67b31ccb7304baadc5481ab1
Furkan Yıldırım <yildirim.frkn@outlook.com>
2025-03-11 14:25:49 +0300

    products and category migration

--- api-project.webApi/Entities/Product.cs
+++ api-project.webApi/Entities/Product.cs
@@ -8,5 +8,9 @@
         public string ProductDescription { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public Category Category { get; set; }
     }
 }
